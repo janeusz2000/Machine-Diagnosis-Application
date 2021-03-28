64810a374e1e54d4675eea3595c4d72cac3c6390
Mateusz <Mateusz.krzysztof.janik@gmail.com>
2021-03-28 21:37:30 +0200

    parallel card acquisition and gui

--- lab3App/main.py
+++ lab3App/main.py
@@ -4,20 +4,60 @@ import nidaqmx as ni
 import nidaqmx.stream_readers as streams
 import time
 import keyboard
+import queue
+import threading
+import logging
+
+logging.getLogger().setLevel((logging.INFO))
+
+def acquiringData(que):
+   t = threading.current_thread()
+   name = str(t.getName())
+
+   logging.info(f"ACQUISITION Thread: {name} started!")
+
+   system = ni.system.system.System.local()
+   taskName = system.tasks.task_names[0]
+   logging.info(f"Task Name: {taskName}")
+   prepareTask = ni.system.storage.persisted_task.PersistedTask(taskName)
+   task = prepareTask.load()
+
+   while not keyboard.is_pressed('q'):
+      with LOCK:
+         que.put(np.array(task.read(ni.constants.READ_ALL_AVAILABLE)))
+
+      time.sleep(0.3)
+   task.stop()
+
+   logging.info(f"ACQUISITION Thread: {name} ended!")
+
+def guiThread(que):
+   t = threading.current_thread()
+   name = str(t.getName())
+   logging.info(f"GUI Thread: {name} started!")
+   while not keyboard.is_pressed('q'):
+      with LOCK:
+         if not que.empty():
+            print(que.get())
+      time.sleep(0.2)
+   logging.info(f"GUI Thread: {name} ended!")
 
 
-fs = 25.6e3
-bufferSize = int(fs);
+global LOCK
+LOCK = threading.Lock()
 
-system = ni.system.system.System.local()
-taskName = system.tasks.task_names[0]
-print(f"Task Name: {taskName}")
-prepareTask = ni.system.storage.persisted_task.PersistedTask(taskName)
-task = prepareTask.load()
-while not keyboard.is_pressed('q'):
-   print(np.array(task.read(ni.constants.READ_ALL_AVAILABLE)))
-   time.sleep(0.1)
-task.stop()
+global dataQue
+dataQue = queue.Queue()
+
+threads = list()
+threads.append(threading.Thread(target=acquiringData, args=(dataQue,)))
+threads.append(threading.Thread(target=guiThread, args=(dataQue,)))
+
+for thread in threads:
+   thread.start()
+
+for thread in threads:
+   thread.join()
 
 
 
